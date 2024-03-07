b760fd4a6e10e1b810c2fe7f3b47716e4c4f064e
James1412 <73318218+James1412@users.noreply.github.com>
2024-03-07 12:13:39 -0500

    🍯 autobrightnesscontrol hivebox and repo

--- lib/features/settings/repos/auto_brightness_repo.dart
+++ lib/features/settings/repos/auto_brightness_repo.dart
@@ -1,1 +1,12 @@
-class AutoBrightnessRepository {}
+import 'package:hive/hive.dart';
+import 'package:study_timer/utils/hive_box_const.dart';
+
+final _box = Hive.box(autoBrightnessHiveBoxConst);
+
+class AutoBrightnessRepository {
+  bool? get getAutoBrightness => _box.get(autoBrightnessHiveBoxConst);
+
+  void setAutoBrightness(bool val) {
+    _box.put(autoBrightnessHiveBoxConst, val);
+  }
+}
--- lib/features/settings/view_models/auto_brightness_vm.dart
+++ lib/features/settings/view_models/auto_brightness_vm.dart
@@ -1,13 +1,17 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:study_timer/features/settings/repos/auto_brightness_repo.dart';
 
 class AutoBrightnessViewModel extends Notifier<bool> {
+  final AutoBrightnessRepository _repository = AutoBrightnessRepository();
+
   void changeIsAutoBrightnessControl(bool val) {
     state = val;
+    _repository.setAutoBrightness(val);
   }
 
   @override
   bool build() {
-    return true;
+    return _repository.getAutoBrightness ?? true;
   }
 }
 
